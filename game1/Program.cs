80f70cc1b45b8f95dbc43d2c68f38ecebeddccec
ptimof01 <ptimof01@gmail.com>
2026-03-10 18:02:20 +0300

    class Game

--- game1/Program.cs
+++ game1/Program.cs
@@ -1,20 +1,47 @@
 using Microsoft.VisualBasic;
+static void Main()
+{
 
-Console.WriteLine("Добро пожаловать в сногсшибательную игру 'УльтраМегаГигаХроносАгония: Эпоха Древних Богов и Великих Королевств'!\n");
-Console.WriteLine("1.игра");
-Console.WriteLine("2.магазин");
-Console.WriteLine("3.выйти из игры");
-int choise = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("----Добро пожаловать в сногсшибательную игру 'УльтраМегаГигаХроносАгония: Эпоха Древних Богов и Великих Королевств----'!\n");
+    Console.WriteLine("1.игра");
+    Console.WriteLine("2.магазин");
+    Console.WriteLine("3.выйти из игры");
+    int firstChoise = Convert.ToInt32(Console.ReadLine());
+    Choise(firstChoise);
 
-class Game
+    static void Choise(int choise)
+    {
+        switch (choise)
+        {
+            case 1:
+        
+            break;
+
+            case 2:
+        
+            break;
+
+            case 3:
+                Console.WriteLine("Уверен? y/n");
+                string exitChoise = Console.ReadLine();
+                if(exitChoise == "y")
+                    {
+                        Console.WriteLine("Твоя воля - закон!");
+                        Environment.Exit(0);
+                    }
+        
+            break;
+        }
+    }
+}
+
+
+
+public class Game
 {
     
 }
 
-class Game1
-{
-    
-}
 public class Player
 {
     

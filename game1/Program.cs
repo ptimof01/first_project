0b21399c3cce09d87fa429d773a36bc91e387e26
exxownerpimp <exxownerpimp@gmail.com>
2026-03-10 18:07:30 +0300

    Class Player Class Enemy

--- game1/Program.cs
+++ game1/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualBasic;
 
+
 Console.WriteLine("Добро пожаловать в сногсшибательную игру 'УльтраМегаГигаХроносАгония: Эпоха Древних Богов и Великих Королевств'!\n");
 Console.WriteLine("1.игра");
 Console.WriteLine("2.магазин");
@@ -11,11 +12,36 @@ class Game
     
 }
 
-class Game1
+public class Enemy
 {
-    
-}
+    public string Name {get; set;}
+    public int Health{get; set;}
+    public int AttackPower{get; set;}
+    public int ExpReward{get; set;}
+
+            public Enemy(string name, int health, int attaclpower, int expreward)
+            {
+                
+                
+            }
 public class Player
 {
-    
+    public string Name{get; set;}
+    public int Health {get; set;}
+    public int MaxHealth{get; set;}
+    public int AttackPower{get; set;}
+    public int Exp;
+        public Player(string name)
+    {
+        Health=MaxHealth;
+        AttackPower=30;
+        Exp=0;
+        Name=name;
+    }   
+    public void Attack(Enemy enemy)
+    {
+       
+    }
 }
+}
+//TAKEDMG DIE HEAL 

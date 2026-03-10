6153e6c12b00b439205ba6cd1a2e82176ce3de8d
exxownerpimp <exxownerpimp@gmail.com>
2026-03-10 19:23:59 +0300

    Сlass Player; Class Enemy; Class Items

--- game1/Program.cs
+++ game1/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.VisualBasic;
+using System;
+
 static void Main()
 {
 
     Console.WriteLine("----Добро пожаловать в сногсшибательную игру 'УльтраМегаГигаХроносАгония: Эпоха Древних Богов и Великих Королевств----'!\n");
-    Console.WriteLine("1.игра");
-    Console.WriteLine("2.магазин");
-    Console.WriteLine("3.выйти из игры");
+    Console.WriteLine("1.Игра");
+    Console.WriteLine("2.Магазин");
+    Console.WriteLine("3.Выйти из игры");
     int firstChoise = Convert.ToInt32(Console.ReadLine());
     Choise(firstChoise);
 
@@ -44,34 +46,77 @@ public class Game
 
 public class Enemy
 {
-    public string Name {get; set;}
-    public int Health{get; set;}
-    public int AttackPower{get; set;}
-    public int ExpReward{get; set;}
+    public string Name { get; set; }
+    public int Health { get; set; }
+    public int AttackPower { get; set; }
+    public int ExpReward { get; set; }
 
-            public Enemy(string name, int health, int attaclpower, int expreward)
+            public Enemy(string name, int health, int attackPower, int expReward)
             {
-                
-                
+                Name = name;
+                Health = health;
+                AttackPower = attackPower;
+                ExpReward = expReward;
             }
+             public void Attack(Player player) //бьёт игрок
+            {
+                player.TakeDamage(AttackPower);
+            }
+            public void TakeDamage(int damage) //моб получает урон
+            {
+                Health -= damage;
+            }
+            public bool IsAlive() //проверка жив ли моб
+            {
+                return Health<0;
+            }
+}
 public class Player
 {
-    public string Name{get; set;}
-    public int Health {get; set;}
-    public int MaxHealth{get; set;}
-    public int AttackPower{get; set;}
-    public int Exp;
+    public string Name { get; set; }
+    public int Health { get; set; }
+    public int MaxHealth { get; set; }
+    public int AttackPower { get; set; }
+    public int Gold { get; set; }
         public Player(string name)
     {
-        Health=MaxHealth;
-        AttackPower=30;
-        Exp=0;
-        Name=name;
+        Name = name;
+        MaxHealth = 100;
+        Health = MaxHealth;
+        AttackPower = 30;
+        Gold = 0;
     }   
-    public void Attack(Enemy enemy)
+    public void Attack(Enemy enemy) //атака игрока
     {
-       
+       enemy.TakeDamage(AttackPower);
+    }
+    public void TakeDamage(int damage) //получение урона игроком
+    {
+        Health -= damage;
+    }
+    public void die() //смэрть игрока
+    {
+        Health = 0;
+    }
+    public void Heal(int amount) //отхил игрока
+    {
+        Health += amount;
+        if (Health > MaxHealth)
+        {
+            Health=MaxHealth;
+        }
     }
 }
+public class Item
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public int Effect { get; set; }
+    
+        public Item(string name, int price, int effect)
+    {
+        Name = name;
+        Price = price;
+        Effect = effect;
+    }
 }
-//TAKEDMG DIE HEAL 
